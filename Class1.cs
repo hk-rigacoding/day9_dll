504d0a659954c25e2fdbe37de33c26a2ac0c429b
hk-rigacoding <hk.rigacoding@gmail.com>
2021-05-08 17:19:32 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace day9_dll
+{
+    public class Aritmetika
+    {
+        public int Add (int skaitlis1, int skaitlis2)
+        {
+            return skaitlis1 + skaitlis2;
+        }
+
+        public int Sub(int skaitlis1, int skaitlis2)
+        {
+            return skaitlis1 - skaitlis2;
+        }
+    }
+}
